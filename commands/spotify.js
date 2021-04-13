896fd513288d0011f26df928a75121c4c9a62870
Brad Ganley <bradganley@pm.me>
2021-04-13 14:21:46 -0500

    Adding module boilerplate for spotify

---
+++ commands/spotify.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fetch = require('node-fetch');
+
+module.exports = {
+    name: '',
+	description: '',
+    execute(message, args) {
+        
+	}
+}
