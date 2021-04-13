d07580a38ce86ee937d4b7400274b11ba921a9a5
Brad Ganley <bradart.ink@gmail.com>
2021-04-13 01:43:08 -0500

    Make the links look better?

--- commands/links.js
+++ commands/links.js
@@ -6,11 +6,11 @@ const linkBoi = (mes) => {
     var embed = new Discord.MessageEmbed()
         .setColor(0x91EE60)
         .setTitle('Only Posers Don\'t Click All of These Links')
-        .addField("Spotify", "https://open.spotify.com/show/1IX9Q8fmVEah2ydG0Ase6G?si=or9LD4-RQIiA3tCvAQM_yw", true)
-        .addField("Youtube", "https://youtube.com/channel/UC6MQuKiyM15B35VjLlKqmng", true)
-        .addField("Instagram", "https://instagram.com/onlyposersdontlisten?igshid=11upu55710s40", true)
-        .addField("Facebook", "https://www.facebook.com/OPDL2P/", true)
-        .addField("TikTok", "https://vm.tiktok.com/ZMJGkWdWF/", true)
+        .addField("Spotify", "https://open.spotify.com/show/1IX9Q8fmVEah2ydG0Ase6G?si=or9LD4-RQIiA3tCvAQM_yw")
+        .addField("Youtube", "https://youtube.com/channel/UC6MQuKiyM15B35VjLlKqmng")
+        .addField("Instagram", "https://instagram.com/onlyposersdontlisten?igshid=11upu55710s40")
+        .addField("Facebook", "https://www.facebook.com/OPDL2P/")
+        .addField("TikTok", "https://vm.tiktok.com/ZMJGkWdWF/")
 
         mes.channel.send(embed)
 }
