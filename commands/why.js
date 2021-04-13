7903120ae1a2cb52aed7fba6a2b28a0800748216
Brad Ganley <bradart.ink@gmail.com>
2021-04-13 01:28:26 -0500

    Ok, now I can switch to a feature branch to stop flooding master

--- commands/why.js
+++ commands/why.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'why',
 	description: 'Tell me whyyYYY',
     execute(message, args) {
-        message.reply(`The \"Mango\" in my name is a reference to the fact that mangoes are abnormally high in myrcene, a terpene believed to be responsible for increased THC CB1 reception. Or something. Whatever.`);
+        message.reply(`Ain't nothin' but a heartache`);
 	}
 }
